01b70efb9a37fe551e6ab7e6bd80a80c4de91009
AntonBas <basantonoleg@gmail.com>
2025-06-30 14:07:55 +0200

    Ticket status added

--- src/main/java/ua/lviv/bas/cinema/domain/Ticket.java
+++ src/main/java/ua/lviv/bas/cinema/domain/Ticket.java
@@ -5,12 +5,15 @@ import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import ua.lviv.bas.cinema.domain.enums.TicketStatus;
 
 @Entity
 @Table(name = "tickets")
@@ -34,6 +37,10 @@ public class Ticket {
 	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
+
+	@Column(nullable = false, name = "status")
+	@Enumerated(EnumType.STRING)
+	private TicketStatus ticketStatus;
 
 	public Ticket() {
 		super();
---
+++ src/main/java/ua/lviv/bas/cinema/domain/enums/TicketStatus.java
@@ -0,0 +1,5 @@
+package ua.lviv.bas.cinema.domain.enums;
+
+public enum TicketStatus {
+	ACTIVE, CANCELLED, USED
+}
