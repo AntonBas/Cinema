a663212a9f3b26d51a967a7b3bad3621849917e0
AntonBas <basantonoleg@gmail.com>
2025-10-11 22:01:32 +0200

    Add @query

--- backend/src/main/java/ua/lviv/bas/cinema/dao/PersonRepository.java
+++ backend/src/main/java/ua/lviv/bas/cinema/dao/PersonRepository.java
@@ -3,14 +3,19 @@ package ua.lviv.bas.cinema.dao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import ua.lviv.bas.cinema.domain.Person;
 import ua.lviv.bas.cinema.domain.enums.PersonRole;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
 	boolean existsByNameAndRole(String name, PersonRole role);
 
-	Page<Person> searchPersons(String query, PersonRole role, Pageable pageable);
+	@Query("SELECT p FROM Person p WHERE "
+			+ "(:query IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :query, '%'))) AND "
+			+ "(:role IS NULL OR p.role = :role) " + "ORDER BY p.name")
+	Page<Person> searchPersons(@Param("query") String query, @Param("role") PersonRole role, Pageable pageable);
 }
