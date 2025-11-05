5552e1273d9c7dd9283c9d4ba1a00a8057861585
AntonBas <basantonoleg@gmail.com>
2025-11-05 22:26:31 +0100

    Add new dto to UserMapper

--- backend/src/main/java/ua/lviv/bas/cinema/mapper/UserMapper.java
+++ backend/src/main/java/ua/lviv/bas/cinema/mapper/UserMapper.java
@@ -2,9 +2,11 @@ package ua.lviv.bas.cinema.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import ua.lviv.bas.cinema.domain.User;
 import ua.lviv.bas.cinema.dto.user.request.UserRegistrationRequest;
+import ua.lviv.bas.cinema.dto.user.request.UserUpdateRequest;
 import ua.lviv.bas.cinema.dto.user.response.UserResponse;
 
 @Mapper(componentModel = "spring")
@@ -20,6 +22,16 @@ public interface UserMapper {
 	User toEntity(UserRegistrationRequest dto);
 
 	UserResponse toDto(User user);
+
+	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "tickets", ignore = true)
+	@Mapping(target = "enabled", ignore = true)
+	@Mapping(target = "userRole", ignore = true)
+	@Mapping(target = "password", ignore = true)
+	@Mapping(target = "createdAt", ignore = true)
+	@Mapping(target = "updatedAt", ignore = true)
+	@Mapping(target = "email", ignore = true)
+	void updateUserFromDto(UserUpdateRequest dto, @MappingTarget User user);
 
 	default User toEntityWithPassword(UserRegistrationRequest dto, String encodedPassword) {
 		User user = toEntity(dto);
