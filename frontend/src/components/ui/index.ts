e1578d150f55dc1864f4d3da75eb49dfae286403
AntonBas <basantonoleg@gmail.com>
2025-11-11 00:09:18 +0100

    Add index for ui

---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,5 @@
+export { DeleteConfirmModal } from './DeleteConfirmModal';
+export { Input } from './Input';
+export { LoadingSpinner } from './LoadingSpinner';
+export { Notification } from './Notification';
+export { Pagination } from './Pagination';
