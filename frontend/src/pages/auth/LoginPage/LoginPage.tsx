885f9917ff651afaff07a0fb099b363bcaa86d4b
AntonBas <basantonoleg@gmail.com>
2025-11-10 22:49:46 +0100

    Remove loading for login. Because now loading working with App.tsx

--- frontend/src/pages/auth/LoginPage/LoginPage.tsx
+++ frontend/src/pages/auth/LoginPage/LoginPage.tsx
@@ -1,23 +1,15 @@
 import React, { useEffect } from 'react';
 import { Layout } from '@/components/layout/Layout/Layout';
 import { LoginForm } from '@/components/auth/LoginForm/LoginForm';
-import { useAuth } from '@/hooks/features/auth';
 import styles from './LoginPage.module.css';
 
 export const LoginPage: React.FC = () => {
-  const { token, isLoading } = useAuth();
-
   useEffect(() => {
     document.body.classList.add(styles.loginPageBody);
     return () => {
       document.body.classList.remove(styles.loginPageBody);
     };
   }, []);
-
-  if (token && !isLoading) {
-    window.location.href = '/';
-    return null;
-  }
 
   return (
     <Layout>
