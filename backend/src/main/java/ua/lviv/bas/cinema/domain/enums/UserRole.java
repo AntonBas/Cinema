6e526c6518ba19d1dfc055c74ccc93801427bbd7
AntonBas <basantonoleg@gmail.com>
2025-11-25 20:35:34 +0100

    Add new user roles

--- backend/src/main/java/ua/lviv/bas/cinema/domain/enums/UserRole.java
+++ backend/src/main/java/ua/lviv/bas/cinema/domain/enums/UserRole.java
@@ -6,12 +6,20 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum UserRole {
-	ROLE_USER("User"), ROLE_ADMIN("Administrator");
+	ROLE_USER("User"), ROLE_CASHIER("Cashier"), ROLE_CONTENT_MANAGER("Content Manager"), ROLE_ADMIN("Administrator");
 
 	private final String displayName;
 
 	public boolean isAdmin() {
 		return this == ROLE_ADMIN;
+	}
+
+	public boolean isCashier() {
+		return this == ROLE_CASHIER;
+	}
+
+	public boolean isContentManager() {
+		return this == ROLE_CONTENT_MANAGER;
 	}
 
 	public static UserRole fromString(String role) {
