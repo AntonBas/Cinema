2468d79ff73a976ba98c5e98c614476298320fcb
AntonBas <basantonoleg@gmail.com>
2025-11-28 20:28:16 +0100

    Repository: add @Query for filter

--- backend/src/main/java/ua/lviv/bas/cinema/repository/UserRepository.java
+++ backend/src/main/java/ua/lviv/bas/cinema/repository/UserRepository.java
@@ -2,7 +2,11 @@ package ua.lviv.bas.cinema.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import ua.lviv.bas.cinema.domain.User;
 import ua.lviv.bas.cinema.domain.enums.UserRole;
@@ -14,4 +18,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	boolean existsByEmail(String email);
 
 	long countByUserRole(UserRole userRole);
+
+	@Query("SELECT u FROM User u WHERE "
+			+ "(:search IS NULL OR LOWER(u.email) LIKE LOWER(CONCAT('%', :search, '%')) OR "
+			+ "LOWER(u.firstName) LIKE LOWER(CONCAT('%', :search, '%')) OR "
+			+ "LOWER(u.lastName) LIKE LOWER(CONCAT('%', :search, '%'))) AND "
+			+ "(:role IS NULL OR u.userRole = :role) AND " + "(:enabled IS NULL OR u.enabled = :enabled)")
+	Page<User> findByFilters(@Param("search") String search, @Param("role") UserRole role,
+			@Param("enabled") Boolean enabled, Pageable pageable);
 }
