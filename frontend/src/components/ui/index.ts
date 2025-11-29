1cc1b8ccfc4431ba84b53d81f4f59a3c6cb51114
AntonBas <basantonoleg@gmail.com>
2025-11-29 20:12:35 +0100

    Create confirm modal ui component

--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -1,5 +1,6 @@
 export * from './Badge';
 export * from './Button';
+export * from './ConfirmModal';
 export * from './DeleteConfirmModal';
 export * from './Input';
 export * from './LoadingSpinner';
