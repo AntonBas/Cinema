dbe5bd243a32b4335e46dd852e2568b554e820d2
AntonBas <basantonoleg@gmail.com>
2025-12-02 23:40:00 +0100

    Mapper: update session mapper with new dto

--- backend/src/main/java/ua/lviv/bas/cinema/mapper/SessionMapper.java
+++ backend/src/main/java/ua/lviv/bas/cinema/mapper/SessionMapper.java
@@ -2,16 +2,38 @@ package ua.lviv.bas.cinema.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+
 import ua.lviv.bas.cinema.domain.Session;
 import ua.lviv.bas.cinema.dto.session.request.SessionRequest;
-import ua.lviv.bas.cinema.dto.session.response.SessionResponse;
+import ua.lviv.bas.cinema.dto.session.response.SessionAdminResponse;
+import ua.lviv.bas.cinema.dto.session.response.SessionScheduleResponse;
 
 @Mapper(componentModel = "spring")
 public interface SessionMapper {
 
-	@Mapping(target = "endTime", expression = "java(session.getEndTime())")
-	@Mapping(target = "available", expression = "java(session.isAvailable())")
-	SessionResponse toDto(Session session);
+	@Mapping(target = "movieId", source = "movie.id")
+	@Mapping(target = "movieTitle", source = "movie.title")
+	@Mapping(target = "movieDuration", source = "movie.durationMinutes")
+	@Mapping(target = "hallId", source = "hall.id")
+	@Mapping(target = "hallName", source = "hall.name")
+	@Mapping(target = "hallCapacity", source = "hall.capacity")
+	@Mapping(target = "endTime", ignore = true)
+	@Mapping(target = "available", ignore = true)
+	@Mapping(target = "ticketsSold", ignore = true)
+	@Mapping(target = "totalRevenue", ignore = true)
+	SessionAdminResponse toAdminDto(Session session);
+
+	@Mapping(target = "movieId", source = "movie.id")
+	@Mapping(target = "movieTitle", source = "movie.title")
+	@Mapping(target = "moviePosterFileName", source = "movie.posterFileName")
+	@Mapping(target = "movieAgeRating", source = "movie.ageRating")
+	@Mapping(target = "movieDuration", source = "movie.durationMinutes")
+	@Mapping(target = "hallId", source = "hall.id")
+	@Mapping(target = "hallName", source = "hall.name")
+	@Mapping(target = "endTime", ignore = true)
+	@Mapping(target = "availableSeats", ignore = true)
+	@Mapping(target = "hallCapacity", ignore = true)
+	SessionScheduleResponse toScheduleDto(Session session);
 
 	@Mapping(target = "id", ignore = true)
 	@Mapping(target = "movie", ignore = true)
