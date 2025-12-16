8024c7ec66c6719c3220dbe015b690be395651f6
AntonBas <basantonoleg@gmail.com>
2025-12-16 13:02:38 +0100

    Remove Dotenv setting

--- backend/src/main/java/ua/lviv/bas/cinema/CinemaApplication.java
+++ backend/src/main/java/ua/lviv/bas/cinema/CinemaApplication.java
@@ -4,17 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @EnableScheduling
 @SpringBootApplication
 public class CinemaApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-
 		SpringApplication.run(CinemaApplication.class, args);
 	}
-
 }
