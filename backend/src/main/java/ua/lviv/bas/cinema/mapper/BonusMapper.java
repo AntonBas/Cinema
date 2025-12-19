8430248ad29371ef699d09a41a931efb960ed502
AntonBas <basantonoleg@gmail.com>
2025-12-19 23:06:33 +0100

    feat: add bonus system mapper

---
+++ backend/src/main/java/ua/lviv/bas/cinema/mapper/BonusMapper.java
@@ -0,0 +1,30 @@
+package ua.lviv.bas.cinema.mapper;
+
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+import ua.lviv.bas.cinema.domain.BonusCard;
+import ua.lviv.bas.cinema.domain.BonusRules;
+import ua.lviv.bas.cinema.domain.BonusTransaction;
+import ua.lviv.bas.cinema.dto.bonus.request.BonusRulesRequest;
+import ua.lviv.bas.cinema.dto.bonus.response.BonusCardResponse;
+import ua.lviv.bas.cinema.dto.bonus.response.BonusRulesResponse;
+import ua.lviv.bas.cinema.dto.bonus.response.BonusTransactionResponse;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface BonusMapper {
+
+	@Mapping(target = "userId", source = "user.id")
+	BonusCardResponse toBonusCardResponse(BonusCard bonusCard);
+
+	BonusTransactionResponse toBonusTransactionResponse(BonusTransaction transaction);
+
+	BonusRulesResponse toBonusRulesResponse(BonusRules rules);
+
+	@BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+	void updateBonusRulesFromRequest(BonusRulesRequest request, @MappingTarget BonusRules rules);
+}
