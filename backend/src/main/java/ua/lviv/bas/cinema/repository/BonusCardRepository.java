7132cddb984a538c0b6feb084de0dc66c8e817ce
AntonBas <basantonoleg@gmail.com>
2025-12-19 23:06:14 +0100

    Repository: remove Query

--- backend/src/main/java/ua/lviv/bas/cinema/repository/BonusCardRepository.java
+++ backend/src/main/java/ua/lviv/bas/cinema/repository/BonusCardRepository.java
@@ -3,11 +3,10 @@ package ua.lviv.bas.cinema.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import ua.lviv.bas.cinema.domain.BonusCard;
 
-public interface BonusCardRepository extends JpaRepository<BonusCard, Long>, QuerydslPredicateExecutor<BonusCard> {
+public interface BonusCardRepository extends JpaRepository<BonusCard, Long> {
 
 	Optional<BonusCard> findByUserId(Long userId);
 
--- backend/src/main/java/ua/lviv/bas/cinema/repository/BonusRulesRepository.java
+++ backend/src/main/java/ua/lviv/bas/cinema/repository/BonusRulesRepository.java
@@ -3,13 +3,11 @@ package ua.lviv.bas.cinema.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import ua.lviv.bas.cinema.domain.BonusRules;
 import ua.lviv.bas.cinema.domain.enums.BonusTransactionType;
 
-public interface BonusRulesRepository
-		extends JpaRepository<BonusRules, BonusTransactionType>, QuerydslPredicateExecutor<BonusRules> {
+public interface BonusRulesRepository extends JpaRepository<BonusRules, BonusTransactionType> {
 
 	Optional<BonusRules> findByBonusType(BonusTransactionType bonusType);
 
