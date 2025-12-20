29d1a31a52e43ec20b49b443820b328830967240
AntonBas <basantonoleg@gmail.com>
2025-12-20 23:51:30 +0100

    feat: add bonus system domain exceptions

---
+++ backend/src/main/java/ua/lviv/bas/cinema/exception/domain/bonus/BonusCardNotFoundException.java
@@ -0,0 +1,13 @@
+package ua.lviv.bas.cinema.exception.domain.bonus;
+
+import ua.lviv.bas.cinema.exception.core.NotFoundException;
+
+public class BonusCardNotFoundException extends NotFoundException {
+
+	private static final long serialVersionUID = 1L;
+
+	public BonusCardNotFoundException(Long userId) {
+		super(String.format("Bonus card not found for user ID: %d", userId), "BONUS_CARD_NOT_FOUND",
+				String.format("BonusCard entity for user id %d does not exist", userId));
+	}
+}
---
+++ backend/src/main/java/ua/lviv/bas/cinema/exception/domain/bonus/InsufficientPointsException.java
@@ -0,0 +1,16 @@
+package ua.lviv.bas.cinema.exception.domain.bonus;
+
+import org.springframework.http.HttpStatus;
+
+import ua.lviv.bas.cinema.exception.core.BusinessException;
+
+public class InsufficientPointsException extends BusinessException {
+
+	private static final long serialVersionUID = 1L;
+
+	public InsufficientPointsException(Integer available, Integer required) {
+		super(String.format("Not enough bonus points. Available: %d, Required: %d", available, required),
+				"INSUFFICIENT_BONUS_POINTS", HttpStatus.BAD_REQUEST,
+				String.format("User has %d points but needs %d", available, required));
+	}
+}
