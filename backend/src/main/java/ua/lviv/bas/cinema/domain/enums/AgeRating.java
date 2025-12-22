f49486b38d2f70a2af11284949a15660fab6dde8
AntonBas <basantonoleg@gmail.com>
2025-12-22 20:38:42 +0100

    Refactor enums

--- backend/src/main/java/ua/lviv/bas/cinema/domain/enums/AgeRating.java
+++ backend/src/main/java/ua/lviv/bas/cinema/domain/enums/AgeRating.java
@@ -3,8 +3,7 @@ package ua.lviv.bas.cinema.domain.enums;
 public enum AgeRating {
 	PEGI_3(3, "Suitable for all ages – no restrictions"),
 	PEGI_7(7, "May contain mild violence/fear scenes for young children"),
-	PEGI_12(12, "Recommended for viewers aged 12 and older"), 
-	PEGI_16(16, "Suitable only for teens aged 16+"),
+	PEGI_12(12, "Recommended for viewers aged 12 and older"), PEGI_16(16, "Suitable only for teens aged 16+"),
 	PEGI_18(18, "Adults only (18+) – restricted content");
 
 	private final int minAge;
--- backend/src/main/java/ua/lviv/bas/cinema/domain/enums/BookingType.java
+++
@@ -1,5 +0,0 @@
-package ua.lviv.bas.cinema.domain.enums;
-
-public enum BookingType {
-	ONLINE, OFFLINE
-}
