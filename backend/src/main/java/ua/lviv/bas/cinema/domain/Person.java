df41070392c13f5ef95a954be84f4d17d55554fe
AntonBas <basantonoleg@gmail.com>
2025-12-23 20:40:10 +0100

    feat(Person): add indexes for name and role fields

--- backend/src/main/java/ua/lviv/bas/cinema/domain/Person.java
+++ backend/src/main/java/ua/lviv/bas/cinema/domain/Person.java
@@ -7,8 +7,10 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -26,11 +28,11 @@ import ua.lviv.bas.cinema.domain.enums.PersonRole;
 @AllArgsConstructor
 @ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@Table(name = "persons")
+@Table(name = "persons", indexes = { @Index(name = "idx_person_name", columnList = "name"),
+		@Index(name = "idx_person_role", columnList = "role") })
 public class Person {
 
 	@Id
-	@EqualsAndHashCode.Include
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
@@ -38,6 +40,7 @@ public class Person {
 	@Column(nullable = false, length = 50)
 	private String name;
 
+	@NotNull
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	private PersonRole role;
