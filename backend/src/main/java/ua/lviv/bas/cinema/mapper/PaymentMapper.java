b4fc2368790e827e05fa1258c64d096db8e15e76
AntonBas <basantonoleg@gmail.com>
2026-01-26 22:55:26 +0100

    update: payment mapper

--- backend/src/main/java/ua/lviv/bas/cinema/mapper/PaymentMapper.java
+++ backend/src/main/java/ua/lviv/bas/cinema/mapper/PaymentMapper.java
@@ -1,7 +1,6 @@
 package ua.lviv.bas.cinema.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import ua.lviv.bas.cinema.domain.Payment;
@@ -9,9 +8,5 @@ import ua.lviv.bas.cinema.dto.payment.response.PaymentLiqPayDataResponse;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PaymentMapper {
-
-	@Mapping(target = "data", ignore = true)
-	@Mapping(target = "signature", ignore = true)
-	@Mapping(target = "paymentUrl", ignore = true)
 	PaymentLiqPayDataResponse toPaymentLiqPayDataResponse(Payment payment);
 }
