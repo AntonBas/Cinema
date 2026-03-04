5c296904957d6bc489be9b3e150a53d5e32c674d
AntonBas <basantonoleg@gmail.com>
2026-03-04 23:07:33 +0100

    feat: add reset.css to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AppRoutes } from '@/routes/AppRoutes';
 import { AuthProvider } from '@/contexts/AuthContext';
+import '@/components/ui/shared/styles/reset.css';
+import './App.css';
 
 function App() {
   return (
