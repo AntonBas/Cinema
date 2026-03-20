432f7161227aa16b3781bf0df93f3209d4d52e87
AntonBas <basantonoleg@gmail.com>
2026-03-20 22:30:20 +0100

    refactor: convert DTO to records

--- backend/src/main/java/ua/lviv/bas/cinema/dto/common/PageResponse.java
+++ backend/src/main/java/ua/lviv/bas/cinema/dto/common/PageResponse.java
@@ -6,50 +6,19 @@ import java.util.stream.Collectors;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class PageResponse<T> {
-	private List<T> content;
-	private int number;
-	private int size;
-	private long totalElements;
-	private int totalPages;
-	private boolean first;
-	private boolean last;
-	private boolean empty;
-	private boolean hasNext;
-	private boolean hasPrevious;
-	private int numberOfElements;
-	private List<SortInfo> sort;
-
-	@Data
-	@Builder
-	@NoArgsConstructor
-	@AllArgsConstructor
-	public static class SortInfo {
-		private String property;
-		private String direction;
-		private boolean ascending;
+public record PageResponse<T>(List<T> content, int number, int size, long totalElements, int totalPages, boolean first,
+		boolean last, boolean empty, boolean hasNext, boolean hasPrevious, int numberOfElements, List<SortInfo> sort) {
+	public record SortInfo(String property, String direction, boolean ascending) {
 	}
 
 	public static <T> PageResponse<T> from(Page<T> page) {
-		return PageResponse.<T>builder().content(page.getContent()).number(page.getNumber()).size(page.getSize())
-				.totalElements(page.getTotalElements()).totalPages(page.getTotalPages()).first(page.isFirst())
-				.last(page.isLast()).empty(page.isEmpty()).hasNext(page.hasNext()).hasPrevious(page.hasPrevious())
-				.numberOfElements(page.getNumberOfElements()).sort(extractSortInfo(page.getSort())).build();
+		return new PageResponse<>(page.getContent(), page.getNumber(), page.getSize(), page.getTotalElements(),
+				page.getTotalPages(), page.isFirst(), page.isLast(), page.isEmpty(), page.hasNext(), page.hasPrevious(),
+				page.getNumberOfElements(), extractSortInfo(page.getSort()));
 	}
 
 	private static List<SortInfo> extractSortInfo(Sort sort) {
-		return sort.stream()
-				.map(order -> SortInfo.builder().property(order.getProperty())
-						.direction(order.getDirection().name().toLowerCase()).ascending(order.isAscending()).build())
-				.collect(Collectors.toList());
+		return sort.stream().map(order -> new SortInfo(order.getProperty(), order.getDirection().name().toLowerCase(),
+				order.isAscending())).collect(Collectors.toList());
 	}
 }
