4563a975c4e37a8213b8d902ee0e5ff959785d22
AntonBas <basantonoleg@gmail.com>
2026-04-07 18:15:07 +0200

    change: movie admin response genre, person imports to info

--- backend/src/main/java/ua/lviv/bas/cinema/dto/movie/response/MovieAdminResponse.java
+++ backend/src/main/java/ua/lviv/bas/cinema/dto/movie/response/MovieAdminResponse.java
@@ -30,11 +30,11 @@ public record MovieAdminResponse(
 
 		@Schema(description = "URL to the movie poster image", example = "/api/movies/1/poster") String posterUrl,
 
-		@Schema(description = "List of genres associated with the movie") List<GenreResponse> genres,
+		@Schema(description = "List of genres associated with the movie") List<GenreInfoResponse> genres,
 
-		@Schema(description = "List of actors in the movie") List<PersonResponse> actors,
+		@Schema(description = "List of actors in the movie") List<PersonInfoResponse> actors,
 
-		@Schema(description = "List of directors of the movie") List<PersonResponse> directors,
+		@Schema(description = "List of directors of the movie") List<PersonInfoResponse> directors,
 
-		@Schema(description = "List of screenwriters of the movie") List<PersonResponse> screenwriters) {
+		@Schema(description = "List of screenwriters of the movie") List<PersonInfoResponse> screenwriters) {
 }
