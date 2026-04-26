0f57e969891f2bd8e2a2b44f989250e3efe6664d
AntonBas <basantonoleg@gmail.com>
2026-04-26 17:20:24 +0200

    feat: add toTicketCashierResponse mapping in TicketMapper

--- backend/src/main/java/ua/lviv/bas/cinema/mapper/ticket/TicketMapper.java
+++ backend/src/main/java/ua/lviv/bas/cinema/mapper/ticket/TicketMapper.java
@@ -5,31 +5,43 @@ import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import ua.lviv.bas.cinema.domain.ticket.Ticket;
+import ua.lviv.bas.cinema.dto.ticket.response.TicketCashierResponse;
 import ua.lviv.bas.cinema.dto.ticket.response.TicketResponse;
 import ua.lviv.bas.cinema.repository.ticket.projection.TicketProjection;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.WARN)
 public interface TicketMapper {
 
-	@Mapping(target = "ticketCode", source = "uniqueCode")
-	@Mapping(target = "price", source = "finalPrice")
-	@Mapping(target = "ticketType", source = "ticketType.displayName")
-	@Mapping(target = "movieTitle", source = "booking.session.movie.title")
-	@Mapping(target = "sessionTime", source = "booking.session.startTime")
-	@Mapping(target = "hallName", source = "booking.session.hall.name")
-	@Mapping(target = "row", source = "seatReservation.seat.row")
-	@Mapping(target = "seatNumber", source = "seatReservation.seat.number")
-	@Mapping(target = "qrCodeUrl", ignore = true)
-	TicketResponse toTicketResponse(Ticket ticket);
+    @Mapping(target = "ticketCode", source = "uniqueCode")
+    @Mapping(target = "price", source = "finalPrice")
+    @Mapping(target = "ticketType", source = "ticketType.displayName")
+    @Mapping(target = "movieTitle", source = "booking.session.movie.title")
+    @Mapping(target = "sessionTime", source = "booking.session.startTime")
+    @Mapping(target = "hallName", source = "booking.session.hall.name")
+    @Mapping(target = "row", source = "seatReservation.seat.row")
+    @Mapping(target = "seatNumber", source = "seatReservation.seat.number")
+    @Mapping(target = "qrCodeUrl", ignore = true)
+    TicketResponse toTicketResponse(Ticket ticket);
 
-	@Mapping(target = "ticketCode", source = "uniqueCode")
-	@Mapping(target = "price", source = "finalPrice")
-	@Mapping(target = "ticketType", source = "ticketTypeName")
-	@Mapping(target = "movieTitle", source = "movieTitle")
-	@Mapping(target = "sessionTime", source = "sessionStartTime")
-	@Mapping(target = "hallName", source = "hallName")
-	@Mapping(target = "row", source = "row")
-	@Mapping(target = "seatNumber", source = "seatNumber")
-	@Mapping(target = "qrCodeUrl", ignore = true)
-	TicketResponse toTicketResponse(TicketProjection projection);
+    @Mapping(target = "ticketCode", source = "uniqueCode")
+    @Mapping(target = "price", source = "finalPrice")
+    @Mapping(target = "ticketType", source = "ticketTypeName")
+    @Mapping(target = "movieTitle", source = "movieTitle")
+    @Mapping(target = "sessionTime", source = "sessionStartTime")
+    @Mapping(target = "hallName", source = "hallName")
+    @Mapping(target = "row", source = "row")
+    @Mapping(target = "seatNumber", source = "seatNumber")
+    @Mapping(target = "qrCodeUrl", ignore = true)
+    TicketResponse toTicketResponse(TicketProjection projection);
+
+    @Mapping(target = "ticketType", source = "ticketType.displayName")
+    @Mapping(target = "requiresDocument", source = "ticketType.requiresDocument")
+    @Mapping(target = "documentType", source = "ticketType.documentType")
+    @Mapping(target = "movieTitle", source = "booking.session.movie.title")
+    @Mapping(target = "sessionTime", source = "booking.session.startTime")
+    @Mapping(target = "hallName", source = "booking.session.hall.name")
+    @Mapping(target = "seatRow", source = "seatReservation.seat.row")
+    @Mapping(target = "seatNumber", source = "seatReservation.seat.number")
+    @Mapping(target = "userEmail", source = "user.email")
+    TicketCashierResponse toTicketCashierResponse(Ticket ticket);
 }
