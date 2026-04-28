0b297313db78780cd557dbd68fc94bf6538bac90
AntonBas <basantonoleg@gmail.com>
2026-04-28 22:09:30 +0200

    refactor: remove unused generateQRCodeBase64 and app.qr.size config

--- backend/src/main/java/ua/lviv/bas/cinema/service/integration/qr/QRCodeService.java
+++ backend/src/main/java/ua/lviv/bas/cinema/service/integration/qr/QRCodeService.java
@@ -1,65 +1,49 @@
 package ua.lviv.bas.cinema.service.integration.qr;
 
-import java.io.ByteArrayOutputStream;
-import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.imageio.ImageIO;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.qrcode.QRCodeWriter;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import ua.lviv.bas.cinema.exception.domain.technical.QRCodeGenerationException;
+
+import javax.imageio.ImageIO;
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Service
 public class QRCodeService {
 
-	@Value("${app.qr.code.size:200}")
-	private int qrCodeSize;
+    @Value("${app.qr.code.margin:1}")
+    private int qrCodeMargin;
 
-	@Value("${app.qr.code.margin:1}")
-	private int qrCodeMargin;
+    @Value("${app.qr.code.format:PNG}")
+    private String qrCodeFormat;
 
-	@Value("${app.qr.code.format:PNG}")
-	private String qrCodeFormat;
+    public byte[] generateQRCode(String content, int size) {
+        log.debug("Generating QR code for content: {}", content);
 
-	public byte[] generateQRCode(String content) {
-		return generateQRCode(content, qrCodeSize);
-	}
+        try {
+            Map<EncodeHintType, Object> hints = new HashMap<>();
+            hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+            hints.put(EncodeHintType.MARGIN, qrCodeMargin);
 
-	public byte[] generateQRCode(String content, int size) {
-		log.debug("Generating QR code for content: {}", content);
+            var qrCodeWriter = new QRCodeWriter();
+            var bitMatrix = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, size, size, hints);
+            var bufferedImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
 
-		try {
-			Map<EncodeHintType, Object> hints = new HashMap<>();
-			hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
-			hints.put(EncodeHintType.MARGIN, qrCodeMargin);
+            var baos = new ByteArrayOutputStream();
+            ImageIO.write(bufferedImage, qrCodeFormat, baos);
 
-			var qrCodeWriter = new QRCodeWriter();
-			var bitMatrix = qrCodeWriter.encode(content, BarcodeFormat.QR_CODE, size, size, hints);
-			var bufferedImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
+            return baos.toByteArray();
 
-			var baos = new ByteArrayOutputStream();
-			ImageIO.write(bufferedImage, qrCodeFormat, baos);
-
-			return baos.toByteArray();
-
-		} catch (Exception e) {
-			log.error("Failed to generate QR code: {}", e.getMessage());
-			throw new QRCodeGenerationException("Failed to generate QR code", e);
-		}
-	}
-
-	public String generateQRCodeBase64(String content) {
-		var qrCodeBytes = generateQRCode(content);
-		return Base64.getEncoder().encodeToString(qrCodeBytes);
-	}
+        } catch (Exception e) {
+            log.error("Failed to generate QR code: {}", e.getMessage());
+            throw new QRCodeGenerationException("Failed to generate QR code", e);
+        }
+    }
 }
