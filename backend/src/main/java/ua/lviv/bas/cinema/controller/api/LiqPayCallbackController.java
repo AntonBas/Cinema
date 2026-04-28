238d8343d380bf4b2cc8b6d9e4b74fd25d96d455
AntonBas <basantonoleg@gmail.com>
2026-04-29 00:53:55 +0200

    refactor: remove LiqPayCallbackRequest, simplify callback

--- backend/src/main/java/ua/lviv/bas/cinema/controller/api/LiqPayCallbackController.java
+++ backend/src/main/java/ua/lviv/bas/cinema/controller/api/LiqPayCallbackController.java
@@ -1,17 +1,10 @@
 package ua.lviv.bas.cinema.controller.api;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import ua.lviv.bas.cinema.service.booking.PaymentStatusService;
 
 @Slf4j
@@ -25,12 +18,7 @@ public class LiqPayCallbackController {
 
     @PostMapping("/callback")
     @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "LiqPay callback")
-    @ApiResponse(responseCode = "200", description = "Callback processed successfully")
-    public String handleCallback(HttpServletRequest request) {
-        var data = request.getParameter("data");
-        var signature = request.getParameter("signature");
-
+    public String handleCallback(@RequestParam String data, @RequestParam String signature) {
         log.info("POST /api/liqpay/callback");
 
         if (data == null || signature == null) {
