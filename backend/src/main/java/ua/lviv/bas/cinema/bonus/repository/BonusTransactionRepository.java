5198d573626fdf7eacb9b455c75bd45b397361b0
AntonBas <basantonoleg@gmail.com>
2026-09-03 16:23:42 +0200

    perf: replace triangular running-sum subquery with window function in BonusTransactionRepository.findProjectionsByUserId

--- backend/src/main/java/ua/lviv/bas/cinema/bonus/repository/BonusTransactionRepository.java
+++ backend/src/main/java/ua/lviv/bas/cinema/bonus/repository/BonusTransactionRepository.java
@@ -8,12 +8,30 @@ import org.springframework.data.repository.query.Param;
 import ua.lviv.bas.cinema.bonus.domain.BonusTransaction;
 import ua.lviv.bas.cinema.bonus.repository.projection.BonusTransactionProjection;
 
+@SuppressWarnings({"SqlResolve", "SqlNoDataSourceInspection"})
 public interface BonusTransactionRepository extends JpaRepository<BonusTransaction, Long> {
 
-    @Query("SELECT " + "bt.id as id, " + "bt.type as type, " + "bt.pointsChange as pointsChangeRaw, "
-            + "bt.createdDate as createdAt, "
-            + "(SELECT SUM(t.pointsChange) FROM BonusTransaction t WHERE t.bonusCard = bt.bonusCard AND t.id <= bt.id) as newBalance "
-            + "FROM BonusTransaction bt " + "WHERE bt.bonusCard.user.id = :userId " + "ORDER BY bt.createdDate DESC")
+    @Query(value = """
+            SELECT
+                bt.id as id,
+                bt.type as type,
+                bt.points_change as pointsChangeRaw,
+                bt.created_date as createdAt,
+                SUM(bt.points_change) OVER (
+                    PARTITION BY bt.bonus_card_id
+                    ORDER BY bt.id
+                    ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+                ) as newBalance
+            FROM bonus_transactions bt
+            JOIN bonus_cards bc ON bc.id = bt.bonus_card_id
+            WHERE bc.user_id = :userId
+            ORDER BY bt.created_date DESC
+            """, countQuery = """
+            SELECT COUNT(*)
+            FROM bonus_transactions bt
+            JOIN bonus_cards bc ON bc.id = bt.bonus_card_id
+            WHERE bc.user_id = :userId
+            """, nativeQuery = true)
     Page<BonusTransactionProjection> findProjectionsByUserId(@Param("userId") Long userId, Pageable pageable);
 
     boolean existsByReferenceId(String referenceId);
