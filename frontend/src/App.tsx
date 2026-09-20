f4dd308b8b1f0bdea3b4da2a60aa8b8bfb8ba60a
AntonBas <basantonoleg@gmail.com>
2026-09-20 13:51:57 +0200

    fix: reset scroll position on route change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import { AppRoutes } from '@/routes/AppRoutes';
 import { AuthProvider } from '@/context/AuthProvider';
 import { NotificationProvider } from '@/context/NotificationProvider';
 import { NotificationContainer } from '@/components/ui';
+import { ScrollToTop } from '@/components/layout/ScrollToTop/ScrollToTop';
 import '@/components/ui/shared/styles/reset.css';
 import './App.css';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <NotificationProvider>
         <AuthProvider>
           <NotificationContainer />
