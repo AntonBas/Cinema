2bc3baa7d6907aba4f286b96cd9ba6e9a8bec23e
AntonBas <basantonoleg@gmail.com>
2026-09-22 20:45:35 +0200

    fix: run backend in Europe/Kyiv timezone

--- backend/src/main/java/ua/lviv/bas/cinema/CinemaApplication.java
+++ backend/src/main/java/ua/lviv/bas/cinema/CinemaApplication.java
@@ -1,5 +1,7 @@
 package ua.lviv.bas.cinema;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,7 +10,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class CinemaApplication {
 
+	private static final String CINEMA_TIME_ZONE = "Europe/Kyiv";
+
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone(CINEMA_TIME_ZONE));
 		SpringApplication.run(CinemaApplication.class, args);
 	}
 }
